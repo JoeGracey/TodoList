da7df95fe959989f674f8f2bb61c351ca1e54049
Joe Gracey <joe.gracey@gmail.com>
2020-12-20 22:19:56 -0700

    inputItems

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Just a test");
+        // To-do List
+        // Field to enter items
+        // New item button
+        // Delete item button
+        // Cross off when completed
+        enterItems.inputItems();
+    }
+
+    public static class enterItems {
+
+        public static void inputItems() {
+
+            ArrayList<String> itemsToBeAdded = new ArrayList<>();
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                System.out.print("Please enter an item: ");
+                itemsToBeAdded.add(scanner.next());
+                System.out.println(itemsToBeAdded);
+            }
+        }
     }
 }
